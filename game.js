df351f7bf91175cddf7f937f31bbeb5a9eba10d6
samuel cordova <cordovas1337@gmail.com>
2022-09-17 15:58:58 -0500

    added some starter htmla and set up player objects

---
+++ game.js
@@ -0,0 +1,12 @@
+var playerOneHP = 100;
+var playerOneAtk = 10;
+var playerOneMoney = 10;
+var player1 = {
+    hitpoints: 100,
+    attack: 10,
+    money: 10,
+}
+var player2 = {
+    hitpoints:50,
+    attack:12,
+}
